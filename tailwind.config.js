b658fd0d6ce47c7552a82934858d136c7514ca79
Sachin-Tiwari-93 <111558338+Sachin-Tiwari-93@users.noreply.github.com>
2024-05-31 16:30:38 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Whitney: ["Whitney"],
+        Ginto: ["Ginto"],
+        ggSans: ["ggSans"],
+        GintoNord: ["GintoNord"],
+      },
+      colors: {
+        BgBlue: "#404EED",
+      },
+    },
+  },
+  plugins: [],
+};
